333ad11492278e0bbdfc794f3bbd7c3fec373aa3
cillian64 <cillian64@92798944-4d10-0410-9fab-b14eab478a48>
2006-04-03 09:58:13 +0000

    Removed "Hello World" from the main program, and improved the loop.  (Thanks to ed_ from #lugradio, freenode)

--- tuxcast/tuxcast.cpp
+++ tuxcast/tuxcast.cpp
@@ -8,11 +8,9 @@ int main(void)
 {
 	filelist *myfilelist;
 	
-	cout << "Hello, World" << endl;
-
 	myfilelist = parse("http://curry.podshow.com/?feed=rss2"); // parse takes a filelist pointer, makes a new filelist, and fills it
 
-	for(int i=0; i<myfilelist->numoffiles(); i++)
+	for(int i=0, size=myfilelist->numoffiles(); i<size; i++)
 	{
 		cout << "File found..." << endl;
 		cout << "name is " << myfilelist->getfilename(i) << endl;
