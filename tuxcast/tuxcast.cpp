924e4c90062d1c6e173db0397516f5721a02a37e
cillian64 <cillian64@92798944-4d10-0410-9fab-b14eab478a48>
2006-04-10 12:08:25 +0000

    tuxcast now checks all feeds in the config file

--- tuxcast/tuxcast.cpp
+++ tuxcast/tuxcast.cpp
@@ -12,6 +12,7 @@ int main(void)
 	string temp;
 	FILE *outputfile;
 	CURL *mycurl;
+	configuration myconfig;
 	mycurl = curl_easy_init();
 	if(mycurl == NULL)
 	{
@@ -19,27 +20,31 @@ int main(void)
 		return -1;
 	}
 	
-	myfilelist = parse("http://geekbrief.podshow.com/?feed=rss2");
-	for(int i=0, size=myfilelist->numoffiles(); i<size; i++)
+	myconfig.load();
+	for(int i=0; i<myconfig.numoffeeds; i++)
 	{
-		cout << "File(s) found..." << endl;
-		cout << "name is " << myfilelist->getfilename(i) << endl;
-		cout << "length is " << myfilelist->getlength(i) << endl;
-		cout << "URL is " << myfilelist->getURL(i) << endl;
-		cout << "Download?: (yes/no)" << endl;
-		cin >> temp;
-		if(strcmp(temp.c_str(),"yes") == 0)
+		cout << "Checking feed \"" << myconfig.feeds[i].name << "\"..." << endl;
+		myfilelist = parse(myconfig.feeds[i].address);
+		for(int j=0, size=myfilelist->numoffiles(); j<size; j++)
 		{
-			cout << "Downloading..." << endl;
-			outputfile = fopen(myfilelist->getfilename(i).c_str(), "w");
-			if(outputfile == NULL)
+			cout << "File found..." << endl;
+			cout << "Name is " << myfilelist->getfilename(j) << endl;
+			cout << "Download?: (yes/no)" << endl;
+			cin >> temp;
+			if(strcmp(temp.c_str(),"yes") == 0)
 			{
-				cerr << "Error opening output file" << endl;
+				cout << "Downloading..." << endl;
+				outputfile = fopen(myfilelist->getfilename(j).c_str(), "w");
+				if(outputfile == NULL)
+				{
+					cerr << "Error opening output file" << endl;
+				}
+				curl_easy_setopt(mycurl,CURLOPT_URL,myfilelist->getURL(j).c_str());
+				curl_easy_setopt(mycurl,CURLOPT_WRITEDATA,outputfile);
+				curl_easy_perform(mycurl);
+				fclose(outputfile);
+				cout << "Done." << endl;
 			}
-			curl_easy_setopt(mycurl,CURLOPT_URL,myfilelist->getURL(i).c_str());
-			curl_easy_setopt(mycurl,CURLOPT_WRITEDATA,outputfile);
-			curl_easy_perform(mycurl);
-			fclose(outputfile);
 		}
 	}
 	
