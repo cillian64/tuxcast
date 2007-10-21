ffb782bb9cd8fffa6de7a157e4660b49e84109e9
cillian64 <cillian64@googlemail.com>
2007-10-21 16:26:14 +0000

    Fixed version.cpp

--- version.cpp
+++ version.cpp
@@ -10,15 +10,15 @@ using namespace std;
 
 void version()
 {
-	printf(_("Tuxcast version %s, Copyright (c) 2006-2007 David Turner\n"));
+	printf(_("Tuxcast version %s, Copyright (c) 2006-2007 David Turner\n"), RELEASE_VERSION);
 #ifdef RELEASE_BINARY
-	printf(_("Release binary %s\n"));
+	printf(_("Release binary %s\n"), RELEASE_VERSION);
 #endif
 #ifdef RELEASE_SOURCE
-	printf(_("Release source tarball %s\n"));
+	printf(_("Release source tarball %s\n"), RELEASE_VERSION);
 #endif
 #ifdef SUBVERSION
-	printf(_("From subversion, release %s revision %s\n"));
+	printf(_("From subversion, release %s revision %s\n"), RELEASE_VERSION, SUBVERSION_REVISION);
 #endif
 	printf(_("Tuxcast comes with ABSOLUTELY NO WARANTY; for details see COPYING.\n"));
 	printf(_("This is free software, you are welcome to redistribute it under certain conditions; see COPYING for more details\n"));
