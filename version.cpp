635dd404d0bae3b9d92b14670b29e021d7156d83
cillian64 <cillian64@googlemail.com>
2007-10-21 16:22:38 +0000

    Readied version.cpp for translation

--- version.cpp
+++ version.cpp
@@ -1,21 +1,26 @@
 #include <iostream>
+#include <libintl.h>
+#include <locale.h>
+
 #include "version.h"
 
 using namespace std;
 
+#define _(x) gettext(x)
+
 void version()
 {
-	cout << "Tuxcast version " << RELEASE_VERSION << ", Copyright (c) 2006-2007 David Turner" << endl;
+	printf(_("Tuxcast version %s, Copyright (c) 2006-2007 David Turner\n"));
 #ifdef RELEASE_BINARY
-	cout << "Release binary " << RELEASE_VERSION << endl;
+	printf(_("Release binary %s\n"));
 #endif
 #ifdef RELEASE_SOURCE
-	cout << "Release source tarball " << RELEASE_VERSION << endl;
+	printf(_("Release source tarball %s\n"));
 #endif
 #ifdef SUBVERSION
-	cout << "From subversion, release " << RELEASE_VERSION << " revision " << SUBVERSION_REVISION << endl;
+	printf(_("From subversion, release %s revision %s\n"));
 #endif
-	cout << "Tuxcast comes with ABSOLUTELY NO WARANTY; for details see COPYING." << endl;
-	cout << "This is free software, you are welcome to redistribute it under certain conditions; see COPYING for more details" << endl;
+	printf(_("Tuxcast comes with ABSOLUTELY NO WARANTY; for details see COPYING.\n"));
+	printf(_("This is free software, you are welcome to redistribute it under certain conditions; see COPYING for more details\n"));
 }
 
