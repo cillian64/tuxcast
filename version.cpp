d5bc22c010e6002d1bd7f72214d0435d3d76072c
cillian64 <cillian64@googlemail.com>
2008-02-09 15:10:41 +0000

    Updated TODO, compile flags and copyright dates

--- version.cpp
+++ version.cpp
@@ -1,3 +1,24 @@
+/*
+ * 
+ * This file is part of Tuxcast, "The linux podcatcher"
+ * Copyright (C) 2006-2008 David Turner
+ * 
+ * Tuxcast is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuxcast is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Tuxcast; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ * 
+ */
+
 #include <iostream>
 #include <libintl.h>
 #include <locale.h>
@@ -10,7 +31,7 @@ using namespace std;
 
 void version()
 {
-	printf(_("Tuxcast version %s, Copyright (c) 2006-2007 David Turner\n"), RELEASE_VERSION);
+	printf(_("Tuxcast version %s, Copyright (c) 2006-2008 David Turner\n"), RELEASE_VERSION);
 #ifdef RELEASE_BINARY
 	printf(_("Release binary %s\n"), RELEASE_VERSION);
 #endif
