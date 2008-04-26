e1690dd0c052e500000bf531cb7b39da07b7f0a6
cillian64 <cillian64@92798944-4d10-0410-9fab-b14eab478a48>
2008-04-26 14:40:45 +0000

    Added AUTHORS

--- trunk/version.h
+++ trunk/version.h
@@ -12,7 +12,7 @@
 
 // These are defined for subversion checkouts
 #define SUBVERSION
-#define SUBVERSION_REVISION "187"
+#define SUBVERSION_REVISION "189"
 // #define SUBVERSION_BRANCH "branchname"
 
 void version();
