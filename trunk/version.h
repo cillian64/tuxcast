7345f60a262796da68d1e72694bc6374a6d5c1af
cillian64 <cillian64@92798944-4d10-0410-9fab-b14eab478a48>
2008-05-21 16:40:54 +0000

    * Fixed typos in previous commit. :(

--- trunk/version.h
+++ trunk/version.h
@@ -12,7 +12,7 @@
 
 // These are defined for subversion checkouts
 #define SUBVERSION
-#define SUBVERSION_REVISION "202"
+#define SUBVERSION_REVISION "203"
 // #define SUBVERSION_BRANCH "branchname"
 
 void version();
