107043a362440ec4c2ad237e02bf982c1cfc99f0
cillian64 <cillian64@googlemail.com>
2008-05-24 20:12:50 +0000

    * Updated documentation for the incorrectmime hook

--- version.h
+++ version.h
@@ -12,7 +12,7 @@
 
 // These are defined for subversion checkouts
 #define SUBVERSION
-#define SUBVERSION_REVISION "204"
+#define SUBVERSION_REVISION "205"
 // #define SUBVERSION_BRANCH "branchname"
 
 void version();
